0310aac8feb231e0934f681471f3fe41dc5b6fcf
Jariel Floriano <jariel.floriano@gmail.com>
2021-06-15 01:57:09 -0300

    Estrutura

---
+++ common/services/mongoose.service.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+let count = 0;
+// qmsAh-6DV.KQ8zc
+const options = {
+    autoIndex: false, // Don't build indexes
+    poolSize: 10, // Maintain up to 10 socket connections
+    // If not connected, return errors immediately rather than waiting for reconnect
+    bufferMaxEntries: 0,
+    // all other approaches are now deprecated by MongoDB:
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+    
+};
+const connectWithRetry = () => {
+    console.log('MongoDB connection with retry')
+    mongoose.connect("mongodb://koba:qmsAh-6DV.KQ8zc@cluster0.m8ffs.mongodb.net/kobacode?retryWrites=true&w=majority", options).then(()=>{
+        console.log('MongoDB is connected')
+    }).catch(err=>{
+        console.log('MongoDB connection unsuccessful, retry after 5 seconds. ', ++count);
+        setTimeout(connectWithRetry, 5000)
+    })
+};
+
+connectWithRetry();
+
+exports.mongoose = mongoose;
